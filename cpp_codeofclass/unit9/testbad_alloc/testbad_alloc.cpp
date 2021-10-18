dfc3a3096bf2f3d573f62a60da6ea5f7f45d0b9c
zyh-hehe <1798508303@qq.com>
2021-10-18 22:33:09 +0800

    *10.18

--- cpp_codeofclass/unit9/testbad_alloc/testbad_alloc.cpp
+++ cpp_codeofclass/unit9/testbad_alloc/testbad_alloc.cpp
@@ -8,10 +8,10 @@ int main(void)
 	try {
 		for (int i = 0; i < 10000; i++)
 		{
-			auto* p = new long long int[2000000000000];
+			auto* p = new long long int[200000000];
 			delete[] p;
 			cout << i << " array" << endl;
-			Sleep(10);
+			//Sleep(10);
 		}
 	}
 	catch (bad_alloc& e)
